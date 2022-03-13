c2ffe6b3e8c712fb6e9fa679ba629a83eda641ea
IT21365232 <99157288+IT21365232@users.noreply.github.com>
2022-03-13 19:06:26 +0000

    wait.i didn't finish this

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -10,3 +10,32 @@ int main() {
 //==========================
    return 0;
 }
+/*#include<iostream>
+using namespace std;
+class Student{
+	private :
+		int studentid;
+		char name[20];
+	
+	public :
+	    void setassignsDetails(int stid,char name);
+		void assignsDetails();
+		
+};
+void Student::setassignsDetails(int sid,char name){
+	studentid=sid;
+	name=name[20];
+}
+void Student::assignsDetails(){
+	   	cout<<"Student id ="<<studentid<<endl<<"Student Name = "<<name[20]<<endl;
+	}
+	int main(){
+		Student s1,s2;
+		
+		
+		s1.setassignsDetails(55, bosdjvb);
+		s1.assignsDetails(); 
+		
+		return 0;
+	}
+*/
